17351a8d794af6763473f4bcec694ee54249e178
PhucKhang1310 <npkhang2005@gmail.com>
2025-09-26 17:06:57 +0700

    feat: import antTable.css in ItemTable component; enhance table styling with improved border and header properties

--- src/components/ItemTable.tsx
+++ src/components/ItemTable.tsx
@@ -14,6 +14,7 @@ import type { FilterDropdownProps } from "antd/es/table/interface";
 import { useRef, useState } from "react";
 import { SearchOutlined } from "@ant-design/icons";
 import Highlighter from "react-highlight-words";
+import "../styles/antTable.css";
 
 interface ItemTableProps {
   data: ItemOptions[];
